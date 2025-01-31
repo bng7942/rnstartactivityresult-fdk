5638906d89de6323261e38747e114957289d403b
unknown <bng49890@gmail.com>
2025-01-31 15:11:40 +0900

    modified:   android/src/main/java/com/anttivuor/rnstartactivityforfdk/RNStartActivityForFdkPackage.java

--- android/src/main/java/com/anttivuor/rnstartactivityforfdk/RNStartActivityForFdkPackage.java
+++ android/src/main/java/com/anttivuor/rnstartactivityforfdk/RNStartActivityForFdkPackage.java
@@ -15,7 +15,7 @@ public class RNStartActivityForFdkPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
-        modules.add(new RNStartActivityForResultModule(reactContext));
+        modules.add(new RNStartActivityForFdkModule(reactContext));
         return modules;
     }
 
